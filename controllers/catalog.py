fb30dfce0b68af0fe07506c2f164a030634aeec0
withanage <dulip.withanage@gmail.com>
2016-01-27 14:23:49 +0100

    Diverse wünsche

--- controllers/catalog.py
+++ controllers/catalog.py
@@ -156,8 +156,7 @@ def book():
         db.representatives.name,
         db.representatives.url,
         orderby=db.representatives.representative_id)
-
-    full_files = db((db.submission_files.submission_id == book_id) & (db.submission_files.genre_id == myconf.take('omp.monograph_type_id'))& (db.submission_files.file_stage > 5)).select(db.submission_files.original_file_name, db.submission_files.submission_id, db.submission_files.genre_id, db.submission_files.file_id, db.submission_files.revision, db.submission_files.file_stage, db.submission_files.date_uploaded , orderby=db.submission_files.file_id)
+    full_files = db((db.submission_files.submission_id == book_id) & (db.submission_files.genre_id == myconf.take('omp.monograph_type_id'))& (db.submission_files.file_stage > 5)& (db.submission_files.assoc_id==db.publication_formats.publication_format_id) ).select(db.submission_files.original_file_name, db.submission_files.submission_id, db.submission_files.genre_id, db.submission_files.file_id, db.submission_files.revision, db.submission_files.file_stage, db.submission_files.date_uploaded , orderby=db.submission_files.file_id,)
 
     for j in press_settings:
         if j.setting_name == 'name':
