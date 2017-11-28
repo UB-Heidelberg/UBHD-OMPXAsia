40b3eacab37ef549d7dbbd500c60cd6c65423bcf
WWW-Team UB Heidelberg <wwwredaktion@ub.uni-heidelberg.de>
2017-11-28 17:35:58 +0100

    Startseite: Neuerscheinung A. Martin, Ankündigung Zotter; Katalogindexseite: Demnächst-Button; Demnächst: Zotter

--- languages/de.py
+++ languages/de.py
@@ -173,6 +173,7 @@
 'For Authors': 'Informationen für Autoren',
 'For technical reasons, statistical data is available from 03.07.2016 onward. ': 'Aufgrund technischer Gegebenheiten beginnt die statistische Erfassung für diesen Titel erst am 07.03.2016',
 'Forms and Validators': 'Forms und Validators',
+'Forthcoming': 'Demnächst',
 'Free Applications': 'Kostenlose Anwendungen',
 'FSK / USK Angabe': 'FSK / USK Angabe',
 'Fügt bei PB automatisch eine letzte Seite mit Druckimpressum ein, Standardwert ist yes bei fehlender Angabe.': 'Fügt bei PB automatisch eine letzte Seite mit Druckimpressum ein, Standardwert ist yes bei fehlender Angabe.',
